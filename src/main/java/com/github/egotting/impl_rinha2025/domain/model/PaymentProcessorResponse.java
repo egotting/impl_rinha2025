30aa083958b7797ed7b701adbe893f3aac923143
egotting <henricodss@gmail.com>
2025-09-01 17:44:36 -0300

    [FEAT]: Implementação do payments-summary

--- src/main/java/com/github/egotting/impl_rinha2025/domain/model/PaymentProcessorResponse.java
+++
@@ -1,6 +0,0 @@
-package com.github.egotting.impl_rinha2025.domain.model;
-
-import java.math.BigDecimal;
-
-public record PaymentProcessorResponse(int totalRequests, BigDecimal totalAmount) {
-}
---
+++ src/main/java/com/github/egotting/impl_rinha2025/domain/service/Interface/IPaymentSummaryService.java
@@ -0,0 +1,4 @@
+package com.github.egotting.impl_rinha2025.domain.service.Interface;
+
+public interface IPaymentSummaryService {
+}
