590d05ea755fb2525a6abd5d1636becfd17e2d02
egotting <henricodss@gmail.com>
2025-09-13 20:20:14 -0300

    [feat] impl reduzindo de 4k para 1k300 de inconsistencias

--- src/main/java/com/github/egotting/impl_rinha2025/core/http/Interface/IRequestPaymentProcessor.java
+++ src/main/java/com/github/egotting/impl_rinha2025/core/http/Interface/IRequestPaymentProcessor.java
@@ -1,13 +1,9 @@
 package com.github.egotting.impl_rinha2025.core.http.Interface;
 
-import com.github.egotting.impl_rinha2025.domain.ENUM.TypePaymentProcessor;
 import com.github.egotting.impl_rinha2025.domain.model.PaymentRequest;
 
 import reactor.core.publisher.Mono;
 
-import java.util.concurrent.CompletableFuture;
-
 public interface IRequestPaymentProcessor {
-    Mono<TypePaymentProcessor> payment(PaymentRequest request);
-    //Mono<Boolean> paymentFallback(PaymentRequest request);
+    Mono<Void> payment(PaymentRequest request);
 }
--- src/main/java/com/github/egotting/impl_rinha2025/core/http/RequestPaymentProcessor.java
+++ src/main/java/com/github/egotting/impl_rinha2025/core/http/RequestPaymentProcessor.java
@@ -1,8 +1,8 @@
 package com.github.egotting.impl_rinha2025.core.http;
 
-import com.github.egotting.impl_rinha2025.core.Executer.Interface.ITaskSimpleConfig;
+import java.time.Duration;
+
 import com.github.egotting.impl_rinha2025.core.http.Interface.IRequestPaymentProcessor;
-import com.github.egotting.impl_rinha2025.domain.ENUM.TypePaymentProcessor;
 import com.github.egotting.impl_rinha2025.domain.model.PaymentRequest;
 import com.github.egotting.impl_rinha2025.domain.repository.Interface.IMemoryPaymentProcessorRepository;
 
@@ -10,15 +10,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
-import java.util.concurrent.CompletableFuture;
+import reactor.core.publisher.Mono;
 
 @Component
 public class RequestPaymentProcessor implements IRequestPaymentProcessor {
 
     private final IMemoryPaymentProcessorRepository _memory;
-    private final ITaskSimpleConfig _exec;
     @Value("${payment.processor.default}")
     private String Default;
     @Value("${payment.processor.fallback}")
@@ -26,49 +25,41 @@ public class RequestPaymentProcessor implements IRequestPaymentProcessor {
     private final WebClient webClient;
 
     public RequestPaymentProcessor(
-            ITaskSimpleConfig _exec,
             WebClient client,
             IMemoryPaymentProcessorRepository memory) {
         this.webClient = client;
-        this._exec = _exec;
         this._memory = memory;
     }
 
     @Override
-    public Mono<TypePaymentProcessor> payment(PaymentRequest request) {
-        String PAYTOSAND = """
-            {
-                "correlationId": "%s",
-                "amount": %s
-            }
-        """.formatted(request.correlationId(), request.amount());
-            return webClient.post()
-            .uri(Default + "/payments")
-            .contentType(MediaType.APPLICATION_JSON)
-            .accept(MediaType.APPLICATION_JSON)
-            .bodyValue(PAYTOSAND)
-            .retrieve()
-            .bodyToMono(String.class)
-            .map(response -> TypePaymentProcessor.DEFAULT)
-            .doOnNext(result -> {
-                            if (result == TypePaymentProcessor.DEFAULT)
-                                _memory.saveDefault(request);
-                })
-            .onErrorResume(err -> {
-                    System.err.println(err.getMessage());
-                    return webClient.post()
-                        .uri(Fallback + "/payments")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .bodyValue(PAYTOSAND)
-                        .retrieve()
-                        .bodyToMono(String.class)
-                        .map(response -> TypePaymentProcessor.FALLBACK)
-                        .doOnNext(result -> {
-                                if (result == TypePaymentProcessor.FALLBACK)
-                                    _memory.saveFallback(request);
-                            })
-                        .onErrorReturn(TypePaymentProcessor.FAILED);
+    public Mono<Void> payment(PaymentRequest request) {
+        return webClient.post()
+                .uri(Default + "/payments")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(request)
+                .retrieve()
+                .bodyToMono(Void.class)
+                .timeout(Duration.ofMillis(2000))
+                .then(_memory.saveDefault(request))
+                .onErrorResume(err -> {
+                    if (err instanceof WebClientResponseException ex
+                            && ex.getStatusCode().is5xxServerError()) {
+                        return webClient.post()
+                                .uri(Fallback + "/payments")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .bodyValue(request)
+                                .retrieve()
+                                .bodyToMono(Void.class)
+                                .timeout(Duration.ofMillis(2000))
+                                .then(_memory.saveFallback(request))
+                                .onErrorResume(er -> {
+                                    System.out.println("Erro no Fallback também");
+                                    return Mono.empty();
+                                });
+                    }
+                    return Mono.empty();
                 });
     }
 }
